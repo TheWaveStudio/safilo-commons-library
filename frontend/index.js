b161831c9880726cc0ed10e4fca133283f8fba36
Francesca Patanè <>
2021-04-15 16:07:05 +0200

    text: validation login

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,8 @@
-import Vue from "vue";
 import Oruga from "@oruga-ui/oruga";
-import { extend } from "vee-validate";
-import * as rules from "vee-validate/dist/rules";
+// import { extend } from "vee-validate";
+// import * as rules from "vee-validate/dist/rules";
 
 export * from "./components";
 export { Oruga };
-export { Vuelidate };
-export { extend };
-export { rules };
+// export { extend };
+// export { rules };
