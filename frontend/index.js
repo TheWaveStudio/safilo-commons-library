096beb0b7f39e26562200f5821329bf936c720cc
Francesca Patanè <>
2021-04-22 17:35:04 +0200

    fix: veevalidate export

--- frontend/index.js
+++ frontend/index.js
@@ -2,8 +2,9 @@ import Oruga from "@oruga-ui/oruga";
 import { extend } from "vee-validate";
 import * as rules from "vee-validate/dist/rules";
 
+const VeeValidate= {rules: rules, extend: extend}
+
 export * from "./components";
 export { Oruga };
 
-export { extend };
-export { rules };
+export { VeeValidate };
