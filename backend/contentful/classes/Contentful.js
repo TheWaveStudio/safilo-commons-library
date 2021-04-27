f490b3d75b62adec4dc9e934a5b81768aa13128a
mariluli <marialuisa.scrofani@gmail.com>
2021-04-27 10:35:05 +0200

    refactor: class Contentful

--- backend/contentful/classes/Contentful.js
+++ backend/contentful/classes/Contentful.js
@@ -1,16 +1,13 @@
 const contentful = require('contentful')
 
 export class Contentful {
-  constructor ({ spaceId, accessToken }) {
-    this.client = contentful.createClient({
-      space: spaceId,
-      accessToken: accessToken
-    })
+  constructor ({ spaceId: space, accessToken }) {
+    this.client = contentful.createClient({ space, accessToken })
   }
 
   getSpace () {
     return this.client.getSpace()
-    .then((space) => {return space})
+    .then((space) => space)
     .catch(console.error)
   }
 }
