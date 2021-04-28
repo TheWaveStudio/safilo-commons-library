b06e6ba1d01f7c132ca83b0f9e3bd97013a02ccb
Francesca Patanè <>
2021-04-28 18:57:58 +0200

    feat: cart

--- backend/shopify/classes/Shopify.js
+++ backend/shopify/classes/Shopify.js
@@ -5,6 +5,15 @@ const { shopifyCall } = require('../adapters/axios')
 const authMutations = require('../mutations/auth')
 const checkoutMutations = require('../mutations/checkout')
 
+/**
+ * Shopify class
+ * @domain String domain
+ * @secretAdmin String secretAdmin
+ * @secretMultipass String secretMultipass
+ * @storefrontToken String storefrontToken
+ * @apiVersion String apiVersion
+ * @returns Shopify instance
+ */
 export class Shopify {
   constructor ({ domain, secretAdmin, secretMultipass, storefrontToken, apiVersion = 'latest' }) {
     this.domain = domain
@@ -21,38 +30,95 @@ export class Shopify {
     return apiVersions[this.apiVersion]
   }
 
+  /**
+   * CreateCustomer function
+   * @req request
+   * @returns Promise response
+   */
   createCustomer (req) {
     const url = this.url('admin')
     const payload = setPayload(entities.CUSTOMER, req.body)
     return this.callStore(url, endpoints.CUSTOMERS, { method: 'POST', payload })
   }
 
+  /**
+   * CreateCheckout function
+   * @req request
+   * @returns Promise response
+   */
   createCheckout (req) {
     const { mutation, variables } = constructGraphQLRequest(req, checkoutMutations.checkoutCreate)
     return this.callStore(this.url('graphql'), endpoints.GRAPHQL, { method: 'POST', mutation, variables })
   }
 
+  /**
+   * CheckoutItemsAdd function
+   * @req request
+   * @returns Promise response
+   */
   checkoutItemsAdd (req) {
     const { mutation } = constructGraphQLRequest(req, checkoutMutations.checkoutLineItemsAdd)
     const variables = req.body
     return this.callStore(this.url('graphql'), endpoints.GRAPHQL, { method: 'POST', mutation, variables })
   }
 
+  /**
+   * CheckoutItemsUpdate function
+   * @req request
+   * @returns Promise response
+   */
+  checkoutItemsUpdate (req) {
+    const { mutation } = constructGraphQLRequest(req, checkoutMutations.checkoutLineItemsUpdate)
+    const variables = req.body
+    return this.callStore(this.url('graphql'), endpoints.GRAPHQL, { method: 'POST', mutation, variables })
+  }
+
+  /**
+   * CheckoutItemsRemove function
+   * @req request
+   * @returns Promise response
+   */
+  checkoutItemsRemove (req) {
+    const { mutation } = constructGraphQLRequest(req, checkoutMutations.checkoutLineItemsRemove)
+    const variables = req.body
+    return this.callStore(this.url('graphql'), endpoints.GRAPHQL, { method: 'POST', mutation, variables })
+  }
+
+  /**
+   * LoginCustomer function
+   * @req request
+   * @returns Promise response
+   */
   loginCustomer (req) {
     const { mutation, variables } = constructGraphQLRequest(req, authMutations.customerAccessTokenCreate)
     return this.callStore(this.url('graphql'), endpoints.GRAPHQL, { method: 'POST', mutation, variables })
   }
 
+  /**
+   * LoginWithMultipass function
+   * @req request
+   * @returns Promise response
+   */
   loginWithMultipass (req) {
     return this.callStore(this.getLoginWithTokenURI(req))
   }
 
+  /**
+   * GenerateCallStore function
+   * @req request
+   * @returns Promise response
+   */
   generateCallStore(secretAdmin, storefrontToken, url, param, options) {
     return (secretAdmin, storefrontToken, url, param, options)
-    ? shopifyCall(secretAdmin, storefrontToken, url, param, options)
-    : (url, param, options) => shopifyCall(secretAdmin, storefrontToken, url, param, options)
+      ? shopifyCall(secretAdmin, storefrontToken, url, param, options)
+      : (url, param, options) => shopifyCall(secretAdmin, storefrontToken, url, param, options)
   }
 
+  /**
+   * GetLoginWithTokenURI function
+   * @req request
+   * @returns Promise response
+   */
   getLoginWithTokenURI (req) {
     const customerData = {
       email: req.body.email,
@@ -61,7 +127,12 @@ export class Shopify {
     const token = this.multipass.encode(customerData)
     return `${getUri(this.domain)('login')}${token}`
   }
-  
+
+  /**
+   * GetProducts function
+   * @req request
+   * @returns Promise response
+   */
   getProducts (req){
     const url = getUri(this.domain, this.version)('admin')
     const payload = setPayload(entities.CUSTOMER, req.body)
--- backend/shopify/mutations/checkout.js
+++ backend/shopify/mutations/checkout.js
@@ -1,33 +1,87 @@
 import gql from 'graphql-tag'
+const checkout = `
+  checkout{
+    id
+    webUrl
+    lineItems(first: 250) {
+      edges {
+        node {
+          id
+          title
+          quantity
+          variant{
+            id 
+            price
+            image {
+              altText
+              originalSrc
+              transformedSrc
+            }
+          }
+        }
+      }
+    }
+  }`;
 
+const checkoutUserErrors = `
+  checkoutUserErrors{
+    code
+    field
+    message
+  }`;
+
+
+/**
+ * Create checkout mutation
+ * @$input {CheckoutCreateInput} payload
+ * @returns graph-tag
+ */
 export const checkoutCreate = gql
   `mutation checkoutCreate($input: CheckoutCreateInput!) {
     checkoutCreate(input: $input) {
-      checkout {
-        id
-        webUrl
-      }
-      checkoutUserErrors {
-        code
-        field
-        message
-      }
+      ${checkout},
+      ${checkoutUserErrors}
     }
   }`
 
+/**
+ * Add line to checkout mutation
+ * @$lineItems [CheckoutLineItemInput!] lines to add to checkout
+ * @$checkoutId ID checkout id (btoa(id) format)
+ * @returns graph-tag
+ */
 export const checkoutLineItemsAdd = gql
   `mutation checkoutLineItemsAdd($lineItems: [CheckoutLineItemInput!]!, $checkoutId: ID!) {
     checkoutLineItemsAdd(lineItems: $lineItems, checkoutId: $checkoutId) {
-      checkout {
-        id
-        webUrl
-      }
-      checkoutUserErrors {
-        code
-        field
-        message
-      }
+      ${checkout},
+      ${checkoutUserErrors}
     }
   }`
 
+/**
+ * Update line to checkout mutation
+ * @$lineItems [CheckoutLineItemInput!] lines to add to checkout
+ * @$checkoutId ID checkout id (btoa(id) format)
+ * @returns graph-tag
+ */
+export const checkoutLineItemsUpdate = gql
+  `mutation checkoutLineItemsUpdate($checkoutId: ID!, $lineItems: [CheckoutLineItemUpdateInput!]!) {
+    checkoutLineItemsUpdate(checkoutId: $checkoutId, lineItems: $lineItems) {
+        ${checkout},
+        ${checkoutUserErrors}
+     }
+  }`
 
+/**
+ * Remove line from checkout mutation
+ * @$lineItemIds [ID] lines id to remove from checkout (btoa(id) format)
+ * @$checkoutId ID checkout id (btoa(id) format)
+ * @returns graph-tag
+ */
+export const checkoutLineItemsRemove = gql
+  `mutation checkoutLineItemsRemove($checkoutId: ID!, $lineItemIds: [ID!]!) {
+     checkoutLineItemsRemove(checkoutId: $checkoutId, lineItemIds: $lineItemIds) {
+       ${checkout},
+       ${checkoutUserErrors}
+    }
+  }`
