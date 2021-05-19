16bf7789394e0508e604e46604fb9d7208c2ddf7
Francesca Patanè <>
2021-05-19 17:30:34 +0200

    WIP: like

---
+++ frontend/components/atoms/product/like.vue
@@ -0,0 +1,22 @@
+<template>
+  <o-button variant="product-like">Ciao<Icon id="heart" />Ciao</o-button>
+</template>
+<script>
+import Icon from '../icon';
+export default{
+  name: 'ProductLike',
+  components: {
+    Icon
+  }
+}
+</script>
+<style lang="scss">
+  .o-btn.o-btn--product-like{
+    .Icon{
+      .inline-svg{
+        height:1.2rem;
+        width:1.2rem;
+      }
+    }
+  }
+</style>
