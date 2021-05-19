54cb98c38af8c9a47ee0f5365771e64f17740846
Francesca Patanè <>
2021-05-19 12:17:45 +0200

    fix: disabled red

--- frontend/components/atoms/button.vue
+++ frontend/components/atoms/button.vue
@@ -90,6 +90,7 @@ export default{
           &.o-btn--white{
             color: rgba($white, 0.32);
           }
+          &.o-btn--secondary,
           &.o-btn--cr_red{
             background-color: rgba($light-grey, 0.32);
             border-color: rgba($light-grey, 0.32);
