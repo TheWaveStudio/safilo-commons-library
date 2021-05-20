3a5260ba33851e07a853734d5ab64733f2b5420b
Francesca Patanè <>
2021-05-20 18:15:33 +0200

    feat: dropdown filter

---
+++ frontend/components/atoms/dropdown.vue
@@ -0,0 +1,90 @@
+<template>
+  <o-dropdown class="Dropdown" :triggers="['hover']" v-model="selectedOptions" multiple aria-role="list">
+    <o-button type="button" slot="trigger">
+      <span class="label">{{ label }}</span>
+      <Icon id="caret-down"></Icon>
+    </o-button>
+    <DropdownItem v-for="item in items" :key="item.value" :item="item"  />
+  </o-dropdown>
+</template>
+<script>
+import Icon from './icon';
+import DropdownItem from './dropdownItem'
+export default{
+  name: 'Dropdown',
+  components: {
+    DropdownItem,
+    Icon
+  },
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      selectedOptions: []
+    }
+  },
+  watch: {
+    selectedOptions: function() {
+      console.log(this.selectedOptions);
+    }
+  }
+}
+</script>
+<style lang="scss">
+.Dropdown {
+  color: $primary;
+  .o-btn{
+    border: 1px solid $white;
+    border-bottom: 0;
+    color: currentColor;
+    @include font-size-line-weight(16,62,400);
+    letter-spacing: 1px;
+    padding: 0 0.8rem;
+    transition: border-color 150ms ease-out;
+    .o-btn__wrapper{
+      & > span{
+        align-items: center;
+        @include flexing(row);
+      }
+    }
+    .Icon{
+      margin-left:0.3rem;
+      .inline-svg{
+        height: 1rem;
+        width: 1rem;
+      }
+    }
+  }
+  .o-drop__menu{
+    border: 1px solid $primary;
+    left:0;
+    max-height: 15.6rem;
+    min-width: 16rem;
+    overflow-y: scroll;
+    padding: 0 0.8rem;
+    position:absolute;
+    top:100%;
+    transform: none;
+  }
+
+  &:hover,
+  &:active,
+  &:focus{
+    .o-btn{
+      border-color: $primary;
+    }
+  }
+  .o-drop__item--active{
+    background-color: transparent;
+    color: currentColor;
+  }
+}
+</style>
---
+++ frontend/components/atoms/dropdownItem.vue
@@ -0,0 +1,57 @@
+<template>
+    <o-dropdown-item class="DropdownItem" aria-role="listitem" :value="item.value"  @click="selected=!selected">
+      <span class="info">
+        <figure class="img-wrapper">
+          <img class="image" :src="item.image.src" :alt="item.image.alt" />
+        </figure>
+        <span class="name">{{ item.label }}</span>
+      </span>
+      <span class="field-wrapper">
+        <o-checkbox v-model="selected"/>
+      </span>
+    </o-dropdown-item>
+</template>
+<script>
+export default{
+  name: 'DropdownItem',
+  props: {
+    item: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      selected: false
+
+    }
+  },
+}
+</script>
+<style lang="scss">
+.DropdownItem {
+  align-items: center;
+  @include flexing(row nowrap);
+  justify-content: space-between;
+  padding: 0.5rem 0;
+
+  .info{
+    align-items: center;
+    @include flexing(row nowrap);
+  }
+  .img-wrapper {
+    height: 1.6rem;
+    margin: 0 0.4rem 0 0;
+    width: 1.6rem;
+    .image {
+      height: 100%;
+      object-fit: contain;
+      object-position: center;
+      width: 100%;
+    }
+  }
+  .o-chk{
+    pointer-events:none;
+  }
+}
+</style>
