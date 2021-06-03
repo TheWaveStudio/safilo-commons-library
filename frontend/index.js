ccf7ff1336913d783155494947d98a76c5c1771d
mariluli <marialuisa.scrofani@gmail.com>
2021-06-03 13:37:42 +0200

    fix: import rules

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 import Oruga from "@oruga-ui/oruga";
 import extend  from "vee-validate";
-import * as rules from "vee-validate/dist/rules.esm";
+import * as rules from "vee-validate/dist/rules";
 
 const VeeValidate= {rules: rules, extend: extend}
 
