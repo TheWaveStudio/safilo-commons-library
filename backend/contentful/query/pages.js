f3d3279ff215643ec578264e6dc19e8befa345a3
Francesca Patanè <>
2021-06-08 17:15:56 +0200

    fix: query

--- backend/contentful/query/pages.js
+++ backend/contentful/query/pages.js
@@ -2,40 +2,41 @@ import gql from 'graphql-tag'
 
 export const pageBySlug = gql`
   query myQuery($slug: String!, $locale: String!) {
-  pageCollection(locale: $locale, where: {slug: $slug}) {
-    items {
-      hero {
-        sliderItemsCollection(limit: 10) {
-          items {
+    pageCollection(locale: $locale, where: {slug: $slug}) {
+      items {
+        seo{
+          titoloSeo
+          descrizioneSeo
+          openGraphImage{
             title
-            subtitle
-            image {
-              title
-              url
-            }
-            ctaText
+            url
           }
         }
-      }
-      products {
-        product
-      }
-      collections {
-        collections
-      }
-      highlightedProducts {
-        product
-      }
-      bottomSlider {
-        sliderItemsCollection(limit: 10) {
-          items {
-            title
-            subtitle
-            image {
-              title
-              url
+        mainContentCollection(limit: 9){
+          items{
+            ... on Slider{
+              sliderItemsCollection(limit: 5){
+                items{
+                  title
+                  subtitle
+                  image{
+                    title
+                    url
+                  }
+                  ctaText
+                  ctaLink
+                }
+              }
             }
-            ctaText
+            ... on Products{
+              ids
+              tag
+              ordinamento
+            }
+            ... on Collections{
+                ids
+                tag
+            }
           }
         }
       }
