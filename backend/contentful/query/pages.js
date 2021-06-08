a705faf99f155b96d89a043e02034099fc487388
Francesca Patanè <>
2021-06-08 17:44:22 +0200

    fix: content page query

--- backend/contentful/query/pages.js
+++ backend/contentful/query/pages.js
@@ -1,46 +1,45 @@
 import gql from 'graphql-tag'
 
 export const pageBySlug = gql`
-  query myQuery($slug: String!, $locale: String!) {
-    pageCollection(locale: $locale, where: {slug: $slug}) {
-      items {
-        seo{
-          titoloSeo
-          descrizioneSeo
-          openGraphImage{
-            title
-            url
-          }
+  query getPageBySlug($slug: String!, $locale: String!) {
+  pageCollection(locale: $locale, where: {slug: $slug}) {
+    items {
+      seo {
+        titoloSeo
+        descrizioneSeo
+        openGraphImage {
+          title
+          url
         }
-        mainContentCollection(limit: 9){
-          items{
-            ... on Slider{
-              sliderItemsCollection(limit: 5){
-                items{
+      }
+      mainContentCollection(limit: 9) {
+        items {
+          __typename
+          ... on Slider {
+            sliderItemsCollection(limit: 5) {
+              items {
+                title
+                subtitle
+                image {
                   title
-                  subtitle
-                  image{
-                    title
-                    url
-                  }
-                  ctaText
-                  ctaLink
+                  url
                 }
+                ctaText
               }
             }
-            ... on Products{
-              ids
-              tag
-              ordinamento
-            }
-            ... on Collections{
-                ids
-                tag
-            }
+          }
+          ... on Products {
+            ids
+            tag
+            ordinamento
+          }
+          ... on Collections {
+            ids
+            tag
           }
         }
       }
     }
   }
-}
+ }
 `
