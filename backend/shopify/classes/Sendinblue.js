6a6eac15235665f3e9256d0acfd7afdaa8e25add
mariluli <marialuisa.scrofani@gmail.com>
2021-06-17 12:59:35 +0200

    feat: newsletter

---
+++ backend/shopify/classes/Sendinblue.js
@@ -0,0 +1,18 @@
+import { httpMethods } from "../../commons/enums/axios"
+import { getSBUri, setSBPayload } from "../utils/sendinblue"
+import { endpoints } from "../enums/sendinblue"
+import { sendinblueCall } from "../../commons/adapters/axios"
+
+export class Sendinblue {
+  constructor ({ apiKey, lists }) {
+    this.apiKey = apiKey
+    this.lists = lists
+  }
+
+  createContact (payload) {
+    const url = getSBUri('v3')
+    payload = setSBPayload(payload, this.lists)
+
+    return sendinblueCall(this.apiKey, url, endpoints.CONTACTS, { method: httpMethods.POST, payload })
+  }
+}
