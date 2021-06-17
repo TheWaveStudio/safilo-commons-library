83c25783ca8ab4b958225a5b91c2d9b0b3329d77
Stefano Soi <devteam.safilo@it.ey.com>
2021-06-17 09:39:16 +0200

    feat: footer links list + footer section title

---
+++ frontend/components/footer/links-list.vue
@@ -0,0 +1,52 @@
+
+<template>
+  <div class="LinksList" :class="{'--pl': site === 'pl'}" itemscope itemtype="http://schema.org/SiteNavigationElement">
+    <SectionTitle :title="title" :site="site"></SectionTitle>
+    <li class="list-item" v-for="(link,index) in list" :key="index" itemprop="url" item>{{link.label}}</li>
+  </div>
+</template>
+<script>
+import SectionTitle from './section-title'
+export default {
+  name: 'LinksList',
+  components: {SectionTitle},
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    list: {
+      type: Array,
+      default:[]
+    },
+    site: {
+      validator: (val) => ['cr', 'pl'].indexOf(val !== -1),
+    }
+  },
+  data() {
+    return {};
+  }
+}
+</script>
+<style lang="scss">
+.LinksList {
+  .list-item{
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: 1px;
+    margin-bottom: 16px;
+    color: $white;
+    list-style: none;
+
+    &:last-of-type{
+      margin-bottom: 0;
+    }
+  }
+
+  &.--pl{
+    .list-item{
+      color: inherit
+    }
+  }
+}
+</style>
---
+++ frontend/components/footer/section-title.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <h4 class="SectionTitle" :class="{'--color-red': site === 'cr'}">{{title}}</h4>
+</template>
+<script>
+
+export default {
+  name: 'SectionTitle',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    site: {
+      validator: (val) => ['cr', 'pl'].indexOf(val !== -1),
+    }
+  },
+  data() {
+    return {};
+  }
+}
+</script>
+<style lang="scss">
+.SectionTitle {
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  margin-bottom: 16px;
+}
+</style>
